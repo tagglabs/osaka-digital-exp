e8e384536942445b3a5d6bee6563f0ad4c091123
Divyansh Chamoli <124081769+divyanshChamoli@users.noreply.github.com>
2025-03-19 18:34:39 +0530

    feat: Add react-hook-form, zod for form handling and validation (#3)

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,11 +1,14 @@
-interface ButtonProps {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   placeholder: string;
 }
 
-export const Button = ({placeholder}: ButtonProps) => {
+export const Button = ({ placeholder, ...props }: ButtonProps) => {
   return (
-    <button className="px-4 py-2 my-1 bg-[#666464] rounded-l-full rounded-r-full text-[15px] text-white">
-        {placeholder}
+    <button 
+      className="px-4 py-2 my-1 bg-[#666464] rounded-l-full rounded-r-full text-[15px] text-white"
+      {...props}
+    >
+      {placeholder}
     </button>
   );
 };
